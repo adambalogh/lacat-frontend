96b083e11bd15b920b0e144d519f31c33b481caa
Adam Balogh <balogh.adam@icloud.com>
2022-09-10 23:51:51 +0400

    semantic ui

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import "react-datepicker/dist/react-datepicker.css";
+import 'semantic-ui-css/semantic.min.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
